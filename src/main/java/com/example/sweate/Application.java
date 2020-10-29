40d20588d5d5a6358f41dcd61e9df8f149065c4b
Pavlovich Kirill <pavlovich.kll@gmail.com>
2020-10-29 14:51:50 +0300

    first commit

---
+++ src/main/java/com/example/sweate/Application.java
@@ -0,0 +1,11 @@
+package com.example.sweate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //находит все контроллеры и тд, чтобы запустить приложение
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
