fabef7e8f27fc928b376f58ef8cc2d09838511a5
Pavlovich Kirill <pavlovich.kll@gmail.com>
2020-10-31 10:08:13 +0300

    Spring Boot Web Application (MVC): visualisation, connected the Bootstrap library to the project.

--- src/main/java/com/example/sweater/repository/MessageRepo.java
+++ src/main/java/com/example/sweater/repository/MessageRepo.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface MessageRepo extends CrudRepository<Message, Integer> {
+public interface MessageRepo extends CrudRepository<Message, Long> {
     //метод для поиска в БД по тэгу
    List<Message> findByTag(String tag);
 }
